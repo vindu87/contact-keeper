89725d952c50e366059ac0f49f32bb231e6ab4e4
Jovan Zivlak <jovanzivlak@Jovans-MacBook-Air.local>
2020-01-28 00:00:17 +0100

    server

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to Contact keeper' }));
+// Connect Database
+connectDB();
 
-//define routes
-app.use('.api/users', require('./routes/users'));
-app.use('.api/auth', require('./routes/auth'));
-app.use('.api/contacts', require('./routes/contacts'));
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
